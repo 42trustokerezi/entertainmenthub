a3e59cb4c81d4d7aff4d214c896bd78fb78f0c2e
Trust Okerezi <trustokerezi42@gmail.com>
2021-06-24 11:13:45 +0100

    first commit

---
+++ src/Pages/Series/Series.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Series = () => {
+    return (
+        <div>
+            <span className="pageTitle">Movie Series</span>
+        </div>
+    )
+}
+
+export default Series
---
+++ src/Pages/Trending/Trending.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export const Trending = () => {
+    return (
+        <div>
+            <span className="pageTitle">Trending</span>
+        </div>
+    )
+}
+
+export default Trending
